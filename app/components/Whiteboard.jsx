5d32ca1837cc4e949f56a86351495821747df8d2
balwinder18 <144101609+balwinder18@users.noreply.github.com>
2024-10-28 15:01:27 +0530

    1

--- app/components/Whiteboard.jsx
+++ app/components/Whiteboard.jsx
@@ -10,34 +10,55 @@ export default function Whiteboard() {
 
   useEffect(() => {
     const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 0.688; // Set canvas width
+    canvas.height = window.innerHeight * 0.8; // Set canvas height
 
 
     const context = canvas.getContext('2d');
     context.strokeStyke = 'black';
     context.lineCap = 'round';
-    context.lineWidth ='5';
-    contextRef = context;
+    context.lineWidth =1;
+    contextRef.current = context;
   
+
+    
   
   }, []);
 
-  const startdrwaing =()=>{
-      setIsdrawing(true);
+  const startdrawing =({nativeEvent})=>{
       
+      const {offsetX ,offsetY} = nativeEvent;
+      contextRef.current.beginPath();
+      contextRef.current.moveTo(offsetX, offsetY);
+      setIsdrawing(true);
   };
 
   const stopdrawing =()=>{
-
+    contextRef.current.closePath();
+    setIsdrawing(false);
   };
 
-  const draw=()=>{
-
+  const draw=({nativeEvent})=>{
+    if (!Isdrawing) return;
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
   };
+  
   
 
 
     
   return (
-    <div></div>
+    <div>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={startdrawing}
+        onMouseUp={stopdrawing}
+        onMouseMove={draw}
+        onMouseLeave={stopdrawing}
+        className="border border-black"
+      />
+    </div>
   )
 }
